c8f481012fc515029ff934664bd03675d1dfbd54
huetteljannick <jannick5@outlook.de>
2026-04-01 11:29:36 +0200

    Changed citbin-dev.sh to use relative paths

--- citbin-dev.sh
+++ citbin-dev.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-# Set path for citbin directory
-CITBIN_PATH="$HOME/Projects/citbin"
-
 # Update infrastructure
-cd "$CITBIN_PATH/infrastructure" || exit
+cd "infrastructure" || exit
 ./update-development.sh
 
 # Start backend
-cd "$CITBIN_PATH/apps/api" || exit
+cd "../apps/api" || exit
 python app.py &
 xdg-open "http://localhost:8000/api/docs"
 
 # Start frontend
-cd "$CITBIN_PATH/apps/web" || exit
+cd "../web" || exit
 npm run dev &
 xdg-open "http://localhost:3000"
